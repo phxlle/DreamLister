ca60cae25861885e5364227272bc56072843f304
Phxlle <philippe.asselbergh@gmail.com>
2017-06-21 21:16:53 +0200

    project commit

--- DreamLister/ItemCell.swift
+++ DreamLister/ItemCell.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class ItemCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item) {
+        
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+        thumb.image = item.toImage?.image as? UIImage
+        
     }
 
 }
