ddf824a710039186771f9b8fc7cbf333353e6757
Phxlle <philippe.asselbergh@gmail.com>
2017-06-21 21:15:51 +0200

    first commit

---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Jonny B on 8/16/16.
+//  Copyright © 2016 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
